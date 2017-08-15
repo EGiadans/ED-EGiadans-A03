4100227938a985cc6b49f5bd54911fb0edfa3061
EGiadans <A01411416@itesm.mx>
2017-08-14 19:34:24 -0500

    Comments

--- ED-EGiadans-A03/src/ed/egiadans/a03/EDEGiadansA03.java
+++ ED-EGiadans-A03/src/ed/egiadans/a03/EDEGiadansA03.java
@@ -17,15 +17,22 @@ public class EDEGiadansA03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Se pide al usuario el numero de elementos que tendra el arreglo
         Scanner tec = new Scanner (System.in); 
         System.out.println("Introduzca el numero de elementos del arreglo ");
         int x = tec.nextInt();
+        //Se instancia un objeto de la clase Secuencial para una busqueda secuencial
         Secuencial os = new Secuencial();
+        //Se crea el arreglo
         os.setV(os.randomV(x));
+        //Se muestran los valores del arreglo 
         os.showV();
+        //Se pide el numero a buscar
         System.out.println("Introduzca el numero cuya posicion desea conocer: ");
         int nD = tec.nextInt();
+        //Se busca el numero deseado
         int p = os.fetch(nD);
+        //Se muestra posicion de ese numero
         os.showF(p);
  
     }
